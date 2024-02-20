976c1d445a95864539399a547cc8c271d39805e3
Jordan Adamson <jordanadamson540@gmail.com>
2024-02-19 19:48:54 -0600

    minor update w info about methods to use

--- while_until_loop_return_value.rb
+++ while_until_loop_return_value.rb
@@ -14,8 +14,28 @@ end
 
 =begin
 problem: the issue is i cannot get until loops correct when i see them. i need
-  more exposure
+  more exposure. I need to throw while loops into the mix as well to keep me on
+  my toes. 
 
 the issue seems to be when the counter is not at 0
+I would say that you need the counter to be in different places within the
+  while/until loop. 
+  -the counter should be declared outside the loop, but inside the method
+  -you will need approximately 5 different methods that would go in the place
+    of size or push
+      -maybe up to 10 considering you are needing two different methods
+        -those methods are
+          #length/size
+          #push/pop unshift/shift
+          #sum (it seems realistic to include another numerical based method)
+          #drop/take
+          #uniq
+          #
+          #
+          #
+          #
+          #
+  -the loop should only be three max lines
+
 
 =end
